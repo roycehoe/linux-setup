22392c7a7d0682f533a5c4865c909c6a59d4bb14
roycehoe <royceinbox@hotmail.com>
2023-12-07 18:34:01 +0800

    refactor: Move version managers dependencies to constants

--- debian/constants.sh
+++ debian/constants.sh
@@ -9,3 +9,40 @@ UI_PACKAGES=(
   "i3-wm"
   "neofetch"
 )
+
+SHELL_PACKAGE="fish"
+
+PYTHON_BUILD_DEPENDENCIES=(
+  "make"
+  "build-essential"
+  "libssl-dev"
+  "zlib1g-dev"
+  "libbz2-dev"
+  "libreadline-dev"
+  "libsqlite3-dev"
+  "wget"
+  "curl"
+  "llvm"
+  "libncurses5-dev"
+  "libncursesw5-dev"
+  "xz-utils"
+  "tk-dev"
+  "libffi-dev"
+  "liblzma-dev"
+  "python3-openssl"
+  "git"
+)
+
+DOCKER_GPG_KEY_DEPENDENCIES=(
+  "ca-certificates"
+  "curl"
+  "gnupg"
+)
+
+DOCKER_PACKAGES=(
+  "docker-ce"
+  "docker-ce-cli"
+  "containerd.io"
+  "docker-buildx-plugin"
+  "docker-compose-plugin"
+)
--- debian/setup_version_managers.sh
+++ debian/setup_version_managers.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 . ./utils.sh
-
-PYTHON_BUILD_DEPENDENCIES=(
-  "make"
-  "build-essential"
-  "libssl-dev"
-  "zlib1g-dev"
-  "libbz2-dev"
-  "libreadline-dev"
-  "libsqlite3-dev"
-  "wget"
-  "curl"
-  "llvm"
-  "libncurses5-dev"
-  "libncursesw5-dev"
-  "xz-utils"
-  "tk-dev"
-  "libffi-dev"
-  "liblzma-dev"
-  "python3-openssl"
-  "git"
-)
-
-DOCKER_GPG_KEY_DEPENDENCIES=(
-  "ca-certificates"
-  "curl"
-  "gnupg"
-)
-
-DOCKER_PACKAGES=(
-  "docker-ce"
-  "docker-ce-cli"
-  "containerd.io"
-  "docker-buildx-plugin"
-  "docker-compose-plugin"
-)
+. ./constants.sh
 
 function _download_asdf() {
   show_installation_banner asdf
