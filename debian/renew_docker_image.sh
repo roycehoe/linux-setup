4c213824d6393eefd1931f21c5d6d05dfacbf36f
roycehoe <royceinbox@hotmail.com>
2023-12-09 09:59:11 +0800

    feat: create docker file specific for debian

--- debian/renew_docker_image.sh
+++ debian/renew_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker rm $(docker ps -a -q  --filter ancestor="mydebianvm") \
-&& docker build -t mydebianvm .\
+&& docker build -f Dockerfile.debian -t mydebianvm .\
 && docker run -it --name mydebiancontainer mydebianvm
