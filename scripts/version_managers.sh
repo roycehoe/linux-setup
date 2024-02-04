69c75a5c5d1fd76bf4e505779b8fe5e20907bc77
roycehoe <royceinbox@hotmail.com>
2024-02-04 12:09:06 +0800

    refactor: refactor version managers script

--- scripts/version_managers.sh
+++ scripts/version_managers.sh
@@ -12,15 +12,23 @@ function _install_asdf() {
   echo "source ~/.asdf/asdf.fish" > ~/.config/fish/config.fish
 }
 
-function _configure_completions() {
+function _configure_asdf_completions() {
   fish -c "mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions"
 }
+
+function _setup_tool_manager {
+  _download_asdf
+  _install_asdf
+  _configure_asdf_completions
+}
+
 
 function _setup_nodejs {
   fish -c "asdf plugin add nodejs"
   fish -c "asdf install nodejs 20.10.0"
   fish -c "asdf global nodejs 20.10.0"
 }
+
 function _setup_go {
   fish -c "asdf plugin add golang https://github.com/asdf-community/asdf-golang.git"
   fish -c "asdf install golang 1.21.4"
@@ -28,15 +36,24 @@ function _setup_go {
 }
 
 function _setup_python() {
-  local package_manager=$1
+  local system_package_manager=$1
 
   for package in ${PYTHON_BUILD_DEPENDENCIES[@]}; do
-    install_package_with_banner $package_manager $package
+    install_package_with_banner $system_package_manager $package
   done
   fish -c "asdf plugin add python"
   fish -c "asdf install python 3.9.0"
   fish -c "asdf global python 3.9.0"
 }
+
+function _setup_runtime_env {
+  local system_package_manager=$1
+
+  _setup_nodejs
+  _setup_python $system_package_manager
+  _setup_go
+}
+
 
 function _setup_poetry() {
   fish -c "curl -sSL https://install.python-poetry.org | python3 -"
@@ -44,87 +61,37 @@ function _setup_poetry() {
 }
 
 function _setup_npm() {
-  local package_manager=$1
+  local system_package_manager=$1
 
-  install_package_with_banner $package_manager npm
+  install_package_with_banner $system_package_manager npm
 }
 
 function _setup_pnpm() {
   fish -c "curl -fsSL https://get.pnpm.io/install.sh | sh -"
 }
 
+function _setup_runtime_package_manager {
+  local system_package_manager=$1
+
+  _setup_poetry
+  _setup_npm $system_package_manager
+  _setup_pnpm
+}
+
+
 function _setup_ghq() {
   fish -c "go install github.com/x-motemen/ghq@latest"
 }
 
-function _setup_version_manager {
-  _download_asdf
-  _install_asdf
-  _configure_completions
-}
-
-function _setup_runtime_env {
-  local package_manager=$1
-
-  _setup_nodejs
-  _setup_python $package_manager
-  _setup_go
-}
-
-function _setup_runtime_package_manager {
-  local package_manager=$1
-
-  _setup_poetry
-  _setup_npm $package_manager
-  _setup_pnpm
-}
-
-function _setup_project_manager {
+function _setup_repo_manager {
   _setup_ghq
 }
 
-# function _setup_asdf {
-#   local package_manager=$1
-
-#   _download_asdf
-#   _install_asdf
-#   _configure_completions
-#   _setup_nodejs
-#   _setup_python $package_manager
-#   _setup_go
-# }
-
-# function setup_version_managers() {
-#   local package_manager=$1
-
-#   _setup_asdf $package_manager
-#   _setup_poetry
-#   _setup_npm $package_manager
-#   _setup_pnpm
-#   _setup_ghq
-# }
-
 function setup_version_managers() {
-  local package_manager=$1
+  local system_package_manager=$1
 
-  _setup_version_manager
-  _setup_runtime_env $package_manager
-  _setup_runtime_package_manager $package_manager
-  _setup_project_manager
+  _setup_tool_manager
+  _setup_runtime_env $system_package_manager
+  _setup_runtime_package_manager $system_package_manager
+  _setup_repo_manager
 }
-
-
-# function setup_tool_manager() {
-# }
-
-# function setup_tool_manager_plugins() {
-# }
-
-# function setup_programming_language_project_manager() {
-# }
-
-# function setup_repository_manager() {
-# }
-
-# function setup_programming_language_version_manager() {
-# }
